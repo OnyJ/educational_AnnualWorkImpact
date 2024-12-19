f91c657172f0bb123ff2ac37db8b21f76c48b9e0
Josselin <julien.josselin75@hotmail.fr>
2024-12-19 13:50:52 +0100

    Edit main: add filtered columns for frequentation & work_time

--- main.py
+++ main.py
@@ -18,8 +18,23 @@ def get_interesting_columns(df, columns):
     return pd.DataFrame()
   return df[columns]
 
+work_time_file = get_clean_work_time('data/temps-de-travail-annuel-depuis-1851.csv')
+work_time_columns = [
+  "Date",
+  "Temps annuel de travail (SNCF)",
+  "Temps annuel de travail (France)"  
+  ]
+work_time = get_interesting_columns(work_time_file, work_time_columns)
 
-frequentation = pd.read_csv("./data/frequentation-gares.csv")
+frequentation_file = pd.read_csv("./data/frequentation-gares.csv", sep=";")
+frequentation_columns = [
+  "Nom de la gare",
+  "Code postal",
+  "Total Voyageurs + Non voyageurs 2017",
+  "Total Voyageurs + Non voyageurs 2018"
+  ]
+frequentation = get_interesting_columns(frequentation_file, frequentation_columns)
+
 
 print(work_time)
 print(frequentation)
