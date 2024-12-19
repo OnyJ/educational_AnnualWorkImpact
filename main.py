4abce19bd05b4f53273bff13d61123c2d29e4f00
Josselin <julien.josselin75@hotmail.fr>
2024-12-19 13:24:52 +0100

    Edit main.py: create get_interesting_columns()

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ def get_clean_work_time(work_time_path):
                   'Temps annuel de travail (France)': int})
         .assign(Commentaires=lambda x: x['Commentaires'].str.strip()))
 
-work_time = get_clean_work_time('data/temps-de-travail-annuel-depuis-1851.csv')
+def get_interesting_columns(df, columns):
+  missing_columns = [col for col in columns if col not in df.columns]
+  if missing_columns:
+    print(f"Columns missing")
+    return pd.DataFrame()
+  return df[columns]
+
 
 frequentation = pd.read_csv("./data/frequentation-gares.csv")
 
